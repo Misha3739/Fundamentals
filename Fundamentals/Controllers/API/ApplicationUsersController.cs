5a2c1c16fa8a60d298844c3c9c894e6600bc907d
Удот Михаил <mihail3739@live.com>
2017-05-18 14:35:10 +0300

    Update roles functionality added

--- Fundamentals/Controllers/API/ApplicationUsersController.cs
+++ Fundamentals/Controllers/API/ApplicationUsersController.cs
@@ -1,11 +1,14 @@
 ﻿using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
 using AutoMapper;
 using Fundamentals.Models.Authorization;
+using Fundamentals.Models.DBContext;
 using Fundamentals.Models.DTO;
 using Fundamentals.Utility;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace Fundamentals.Controllers.API
 {
@@ -21,5 +24,37 @@ namespace Fundamentals.Controllers.API
             var allUsers = _dbContext.Users.Where(x=>!(x.RoleApproved && x.ClaimedRoleId == superAdminRole.Id)).Select(Mapper.Map<ApplicationUser, AspNetUserDto>);
             return Ok(pendingAuthorization ? allUsers.Where(x => x.RoleApproved == false) : allUsers);
         }
+
+        [HttpPut]
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutApplicationUsers([FromBody] ApplicationUserRequest requestParams)
+        {
+            if (requestParams != null)
+            {
+                var manager =
+               new ApplicationUserManager(new UserStore<ApplicationUser>(_dbContext))
+               {
+                   PasswordHasher = new FundamentalsPasswordHasher()
+               };
+
+                var user = _dbContext.Users.Find(requestParams.Id);
+                var role = _dbContext.Roles.Single(x => x.Id == requestParams.AppliedRoleId);
+                user.ClaimedRoleId = requestParams.AppliedRoleId;
+                user.RoleApproved = true;
+
+                await _dbContext.SaveChangesAsync();
+                await manager.AddToRoleAsync(requestParams.Id, role.Name);
+
+                return Ok();
+            }
+            return BadRequest("Invalid parameters");
+        }
+    }
+
+    public class ApplicationUserRequest
+    {
+        public string Id { get; set; }
+
+        public string AppliedRoleId { get; set; }
     }
 }
